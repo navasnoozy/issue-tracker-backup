4b94f97a58ae88fee20928db01be1775c7623956
navasnoozy <166505582+navasnoozy@users.noreply.github.com>
2025-04-06 17:58:50 +0530

    Layout Improvement : NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -9,7 +9,6 @@ import AuthButton from "./components/AuthButton";
 const NavBar = () => {
   const currentPath = usePathname();
   const { data: session, status } = useSession();
-  
 
   const NavLinks = [
     { label: "Dashboard", href: "/" },
@@ -23,34 +22,40 @@ const NavBar = () => {
     signOut();
   };
   return (
-    <Flex className="w-[100%] gap-6 border-b   border-gray-200  shadow-2xs px-6 h-14 items-center  ">
-      <Box className="hover:scale-210 transition-all ">
-        <VscDebugAll />
-      </Box>
-      <ul className="flex gap-6   text-gray-600 items-center">
-        {NavLinks.map((link) => (
-          <li
-            key={link.href}
-            className={`${
-              currentPath === link.href
-                ? "text-black underline underline-offset-6"
-                : ""
-            }  hover:text-black ease-in-out  hover:scale-120 transition-all items-center `}
-          >
-            <Link href={link.href}>{link.label}</Link>
-          </li>
-        ))}
-     
-     
+    <Flex justify={"between"} className="flex justify-between w-full  border-b   border-gray-200  shadow-2xs px-6 h-14   ">
+      <Flex className="gap-6 items-center">
+        <Box className="hover:scale-210 transition-all ">
+          <VscDebugAll />
+        </Box>
+        <ul className="flex gap-6   text-gray-600 items-center">
+          {NavLinks.map((link) => (
+            <li
+              key={link.href}
+              className={`${
+                currentPath === link.href
+                  ? "text-black underline underline-offset-6"
+                  : ""
+              }  hover:text-black ease-in-out  hover:scale-120 transition-all items-center `}
+            >
+              <Link href={link.href}>{link.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </Flex>
 
-        <li>
-         {session  &&  <Avatar size='2' referrerPolicy="no-referrer"  src={session?.user?.image!} radius="full" fallback='A'/>}
-        </li>
+      <Flex className="items-center gap-6">
+        {session && (
+          <Avatar
+            size="2"
+            referrerPolicy="no-referrer"
+            src={session?.user?.image!}
+            radius="full"
+            fallback="A"
+          />
+        )}
 
-        <li>
-          <AuthButton />
-        </li>
-      </ul>
+        <AuthButton />
+      </Flex>
     </Flex>
   );
 };
