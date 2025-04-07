0fbe42c2403712e8e61bffe47c27997bdcded4f3
navasnoozy <166505582+navasnoozy@users.noreply.github.com>
2025-04-07 12:46:23 +0530

    fetch users from db and listed select components

---
+++ app/api/users/route.ts
@@ -0,0 +1,11 @@
+import { prisma } from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+//FETCH ALL USERS
+export async function GET (req: NextRequest){
+     const users = await prisma.user.findMany ({orderBy: {
+        name: 'asc'
+     }});
+
+     return NextResponse.json(users)
+};
--- app/issues/[id]/SelectAssignee.tsx
+++ app/issues/[id]/SelectAssignee.tsx
@@ -1,14 +1,31 @@
 "use client";
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>();
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get<User[]>("/api/users");
+      setUsers(data);
+    };
+    fetchUsers();
+  }, []);
+
   return (
-    <Select.Root size='3' defaultValue="1">
-      <Select.Trigger  />
+    <Select.Root size="3" >
+      <Select.Trigger placeholder="Assignee..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggetions</Select.Label>
-          <Select.Item className="hidden" value="1">Navas</Select.Item>
+          {users?.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
