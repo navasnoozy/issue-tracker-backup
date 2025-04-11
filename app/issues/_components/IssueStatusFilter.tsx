f63d2c466bada02b4e5823db8c277fb6144e2525
navasnoozy <166505582+navasnoozy@users.noreply.github.com>
2025-04-11 12:34:45 +0530

    Filter bug fixes

--- app/issues/_components/IssueStatusFilter.tsx
+++ app/issues/_components/IssueStatusFilter.tsx
@@ -1,11 +1,12 @@
-'use client'
+"use client";
 
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const IssueStatusFilter = () => {
-   const router = useRouter()
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
   const status: { label: string; value?: Status }[] = [
     { label: "All" },
@@ -14,13 +15,23 @@ const IssueStatusFilter = () => {
     { label: "Closed", value: "CLOSED" },
   ];
 
-  const handleChange = (status: string)=>{
-       const query = status && `?status=${status}`;
-       router.push("/issues"+query)
-  }
+  const handleChange = (status: string) => {
+    const params = new URLSearchParams();
+
+    if (status) {
+      params.append("status", status);
+    }
+    if (searchParams.has("sort")) {
+      params.append("sort", searchParams.get("sort")!);
+      params.append("direction", searchParams.get("direction")!);
+    }
+
+    const query = params.size? '?'+ params.toString():'';
+    router.push("/issues" + query);
+  };
 
   return (
-    <Select.Root defaultValue="ALL" onValueChange={handleChange}>
+    <Select.Root defaultValue={searchParams.get('status')||'ALL'} onValueChange={handleChange}>
       <Select.Trigger placeholder="Filter by Status" />
       <Select.Content>
         {status.map((status) => (
