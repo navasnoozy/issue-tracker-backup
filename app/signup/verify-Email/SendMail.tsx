4d81da789c9d5f2bcbafaf36f41d807a0b3b6671
navasnoozy <166505582+navasnoozy@users.noreply.github.com>
2025-04-16 14:16:22 +0530

    Refactored: Send verifiction mail

--- app/signup/verify-Email/SendMail.tsx
+++ app/signup/verify-Email/SendMail.tsx
@@ -1,22 +1,30 @@
 //app/signup/verify-Email/page.tsx
-"use client";
 import useUser from "@/app/hooks/userUser";
-import { Card } from "@radix-ui/themes";
-import { Heading } from "@radix-ui/themes";
-import { useSearchParams } from "next/navigation";
+import { Button, Card, Heading } from "@radix-ui/themes";
+import axios from "axios";
 
 const VerifyEmail = ({ userId }: { userId: string | null }) => {
   const { data: user } = useUser(userId);
 
+  const handleClick = async ()=>{
+         const res = axios.post('/api/users/emailVerification',user)
+  }
+
   return (
-    <Card className="max-w-md bg-green-200 !space-y-2">
+    <Card className="max-w-md bg-green-200  !space-y-3">
       <Heading color="gray">Welcome {user?.name?.toUpperCase()}</Heading>
       <Heading size="3">
-        The verification link has been sent to your email. Please check your
-        inbox and complete the verification.
+      Click the button below to send a verification link to your email address.
       </Heading>
+      <Button onClick={handleClick} className="w-sm">Verify Email</Button>
     </Card>
   );
 };
 
 export default VerifyEmail;
+{
+  /* <Heading size="3">
+        The verification link has been sent to your email. Please check your
+        inbox and complete the verification.
+      </Heading> */
+}
--- app/signup/verify-Email/page.tsx
+++ app/signup/verify-Email/page.tsx
@@ -1,24 +1,17 @@
 //app/signup/verify-Email/page.tsx
-'use client'
+"use client";
 import useUser from "@/app/hooks/userUser";
 import { Card } from "@radix-ui/themes";
 import { Heading } from "@radix-ui/themes";
 import { useSearchParams } from "next/navigation";
 import VerifyEmail from "./SendMail";
 
-
 const page = () => {
-   const searchParams = useSearchParams ();
-   const userId = searchParams.get('userId')
-   const token = searchParams.get('token');
-   
+  const searchParams = useSearchParams();
+  const userId = searchParams.get("userId");
+  const token = searchParams.get("token");
 
-   
-
-
-  return (
-       <VerifyEmail userId={userId} />
-  );
+  return <VerifyEmail userId={userId} />;
 };
 
 export default page;
