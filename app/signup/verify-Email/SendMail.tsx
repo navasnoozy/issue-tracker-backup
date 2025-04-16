efa166489d2d6e0c8d4a1e5f9b4c523169206214
navasnoozy <166505582+navasnoozy@users.noreply.github.com>
2025-04-16 14:02:57 +0530

    Refactored: Verify email page

---
+++ app/signup/verify-Email/SendMail.tsx
@@ -0,0 +1,22 @@
+//app/signup/verify-Email/page.tsx
+"use client";
+import useUser from "@/app/hooks/userUser";
+import { Card } from "@radix-ui/themes";
+import { Heading } from "@radix-ui/themes";
+import { useSearchParams } from "next/navigation";
+
+const VerifyEmail = ({ userId }: { userId: string | null }) => {
+  const { data: user } = useUser(userId);
+
+  return (
+    <Card className="max-w-md bg-green-200 !space-y-2">
+      <Heading color="gray">Welcome {user?.name?.toUpperCase()}</Heading>
+      <Heading size="3">
+        The verification link has been sent to your email. Please check your
+        inbox and complete the verification.
+      </Heading>
+    </Card>
+  );
+};
+
+export default VerifyEmail;
--- app/signup/verify-Email/page.tsx
+++ app/signup/verify-Email/page.tsx
@@ -4,29 +4,21 @@ import useUser from "@/app/hooks/userUser";
 import { Card } from "@radix-ui/themes";
 import { Heading } from "@radix-ui/themes";
 import { useSearchParams } from "next/navigation";
+import VerifyEmail from "./SendMail";
 
 
-const VerifyEmail = () => {
+const page = () => {
    const searchParams = useSearchParams ();
    const userId = searchParams.get('userId')
    const token = searchParams.get('token');
    
-    
+
    
 
-   const {data:user} = useUser(userId)
-  return (
-    <Card className="max-w-md bg-green-200 !space-y-2">
-      <Heading color="gray">Welcome {user?.name?.toUpperCase()}</Heading>
-      <Heading size='3' >
-        The verification link has been sent to your email. Please check your
-        inbox and complete the verification.
-        
-      </Heading>
 
-      
-    </Card>
+  return (
+       <VerifyEmail userId={userId} />
   );
 };
 
-export default VerifyEmail;
+export default page;
