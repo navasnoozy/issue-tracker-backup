7e9c1f9a08bb73605e3b905f689f2dd816152acb
navasnoozy <166505582+navasnoozy@users.noreply.github.com>
2025-04-20 16:28:35 +0530

    Hide singup and singinin link from dropdownmenu for authorized user

--- app/components/navbar/MobileMenu.tsx
+++ app/components/navbar/MobileMenu.tsx
@@ -4,15 +4,27 @@
 import { DropdownMenu, Button } from '@radix-ui/themes';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import Link from 'next/link';
+import { useSession } from 'next-auth/react';
 
-const DropDownLinks = [
-  { label: 'Dashboard', href: '/' },
-  { label: 'Issues', href: '/issues' },
-  { label: 'Create Account', href: '/auth?formType=signup' },
-  { label: 'Signin', href: '/auth?formType=signin' },
-];
+
 
 const MobileMenu = () => {
+  const {status} = useSession();
+
+  const commonLinks = [
+    { label: 'Dashboard', href: '/' },
+    { label: 'Issues', href: '/issues' },
+  ];
+
+  const authLinks = [
+    { label: 'Create Account', href: '/auth?formType=signup' },
+    { label: 'Signin', href: '/auth?formType=signin' },
+  ];
+
+  const DropDownLinks = status === 'authenticated' ? commonLinks : [...commonLinks,...authLinks];
+  
+
+
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger>
